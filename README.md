c5b4ed05b01242728c021315dbb14b68b9b5f410
Zach Olivare <zach@olivare.net>
2024-05-24 10:31:02 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## SkySlope React Interview Question
+
+```bash
+git clone https://github.com/0livare/skyslope-interview-react.git
+cd skyslope-interview-react
+npm run dev
+```
---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import './App.css';
+
+/*
+  Question: 
+  Create a custom React hook called useLocalStorage that syncs a state 
+  variable with the browser's local storage.
+
+  The hook should accept a key and a default value and handle setting 
+  and retrieving the value from local storage.
+
+  Ensure that the component re-renders when the value is updated in 
+  local storage. Illustrate its usage in a React component.
+
+  localStorage.getItem('key');
+  localStorage.setItem('key', value)
+ */
+
+function useLocalStorage(key, defaultValue) {}
+
+function App() {
+  const [count, setCount] = React.useState(0);
+  // const [count, setCount] = useLocalStorage('count', 0);
+
+  return (
+    <div className="card">
+      <button onClick={() => setCount(count + 1)}>count is {count}</button>
+      <p>
+        Edit <code>src/App.jsx</code> and save to see changes
+      </p>
+    </div>
+  );
+}
+
+export default App;
